cf968e4bcda1fe79c2e8d280674e0106a162326f
Alvin Villanueva <alvinvillanueva08.av@gmail.com>
2026-03-24 22:23:52 +0800

    0071826

---
+++ hangman_main.py
@@ -0,0 +1,42 @@
+import random
+from hangman_art import hangman_stages
+from hangman_art import logo
+from hangman_words import word_list
+
+
+
+#this will create list with _ for each letter in the chosen word
+print(logo)
+chosen_word = random.choice(word_list)
+
+word_length = len(chosen_word)
+
+display_list = []
+for _ in range(word_length):
+    display_list += "_"
+
+guess_limit = len(hangman_stages)
+number_of_guesses = guess_limit
+end_of_game = False
+
+while end_of_game == False:
+    user_guess = input("Guess a letter: ").lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == user_guess:
+            display_list[position] = letter
+    if user_guess in display_list:
+        print(display_list)
+    elif "_" not in display_list:
+        end_of_game = True
+        print("You win.")
+    elif user_guess not in chosen_word:
+        print(hangman_stages[number_of_guesses - guess_limit])
+        guess_limit = guess_limit - 1
+        if guess_limit == 0:
+            end_of_game = True
+            print("You lose.")
+
+
+
+
